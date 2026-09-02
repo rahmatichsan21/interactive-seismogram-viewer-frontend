ec3c214cf4887ced1ea4c4a2cbd8370687936a91
rahmatichsan21 <150210985+rahmatichsan21@users.noreply.github.com>
2026-09-03 00:50:54 +0700

    fix frontend processing pipiline

--- src/pages/WaveformViewerPanel.jsx
+++ src/pages/WaveformViewerPanel.jsx
@@ -572,7 +572,7 @@ export default function WaveformViewerPanel({
     }
 
     return order;
-  }, [visibleChannelKey]);
+  }, [visibleChannelKey, processedWaveform]);
 
   const hvsrFamilies = useMemo(() => {
     const families = [];
